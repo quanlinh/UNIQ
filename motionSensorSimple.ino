ab897ad0fa3e228a29b4ff58d7f83cde9f7a85aa
Quan T. Tran <quan_linh_8890@hotmail.com>
2018-05-18 15:33:46 -0400

    the detect code for the sensor

---
+++ motionSensorSimple.ino
@@ -0,0 +1,24 @@
+// source: http://www.learningaboutelectronics.com/Articles/Arduino-motion-sensor-light-circuit.php
+//code for motion sensor circuit
+const int ledPin= 13;
+const int inputPin= 2;
+
+void setup(){
+pinMode(ledPin, OUTPUT);
+pinMode(inputPin, INPUT);
+}
+
+void loop(){
+int value= digitalRead(inputPin);
+
+if (value == HIGH)
+{
+digitalWrite(ledPin, HIGH);
+delay(6000);
+digitalWrite(ledPin, LOW);
+}
+else
+{
+digitalWrite(ledPin, LOW);
+}
+}
